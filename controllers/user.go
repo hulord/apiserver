e61d288eecf70d3700e3e924c9ca7025f596b85b
hu18359780442 <hu18359780442@sohu.com>
2019-02-15 19:46:48 +0800

    1.。

---
+++ controllers/user.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"apiserver/models"
+	"github.com/astaxie/beego"
+)
+
+// UserController operations for App
+type UserController struct {
+	beego.Controller
+}
+
+// URLMapping ...
+func (c *UserController) URLMapping() {
+
+}
+
